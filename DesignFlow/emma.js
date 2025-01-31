630f1434f2904ca05759d7abf7fa863c49abd33a
Adelizmo <AdelAlmalki2006@gmail.com>
2025-01-31 15:35:52 +0300

    Delete DesignFlow directory

--- DesignFlow/emma.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('Footer1').scrollIntoView()
-
-const Toggle_Styling = () => {
-    console.log('test')
-    document.getElementById('FixedStyledElements').classList.toggle('Open')
-}
